3a2198e1f89e8ab789d50d1fb304fff6a426cdae
Greg Chambers <gregc@unity3d.com>
2024-08-01 22:33:51 -1000

    HWGW Batcher v1 complete

--- src/hawaii/batch_hack.ts
+++ src/hawaii/batch_hack.ts
@@ -1,10 +1,11 @@
 import { NS } from '@ns';
 
 export async function main(ns: NS) {
-  if (ns.args.length != 2) {
-    ns.tprint("Usage: batch_hack.js [server_name] [batch_number]")
+  if (ns.args.length != 3) {
+    ns.tprint("Usage: batch_hack.js [server_name] [ms_delay] [batch_number]")
     ns.exit()
   }
+  await ns.sleep(+ns.args[1].valueOf())
   await ns.hack(ns.args[0].toString())
-  ns.tprint("Finished hack against " + ns.args[0].toString() + " in batch number" + ns.args[1].toString())
+  ns.print("Finished hack against " + ns.args[0].toString() + " in batch number" + ns.args[2].toString())
 }
--- src/hawaii/batcher.ts
+++ src/hawaii/batcher.ts
@@ -1,5 +1,31 @@
 import { NS } from '@ns';
-import { ServersWithRam, ServersWithMoney } from './utils';
+import { ServersWithRam} from './utils';
+
+const stepDelay = 20
+
+async function runOnServers(
+  ns: NS,
+  scriptPath: string,
+  targetServer: string,
+  batchId: number,
+  msDelay: number,
+  threads: number
+){
+  let ramUsage = ns.getScriptRam(scriptPath)
+  let allServersWithRam = ns.getPurchasedServers().concat(ServersWithRam)
+  let scriptsLeftToAllocate = threads
+  while (scriptsLeftToAllocate > 0) {
+    for (let serverName of allServersWithRam) {
+      let server = ns.getServer(serverName)
+      if ((server.maxRam - server.ramUsed) > ramUsage) {
+        ns.exec(scriptPath, serverName, 1, targetServer, msDelay, batchId)
+        scriptsLeftToAllocate -= 1
+        break
+      }
+    }
+    await ns.sleep(stepDelay)
+  }
+}
 
 export async function main(ns: NS) {
   if (ns.args.length != 1) {
@@ -16,11 +42,75 @@ export async function main(ns: NS) {
     "/hawaii/batch_hack.js",
     "/hawaii/batch_weaken.js"
   ]
-  for (let server of ServersWithRam) {
-
+  let allServersWithRam = ns.getPurchasedServers().concat(ServersWithRam)
+  for (let server of allServersWithRam) {
     if (!ns.scp(scripts, server, "home")){
-      ns.tprint("Could not copy HGW scripts to " + server + "! Abandoning batcher.")
+      ns.print("Could not copy HGW scripts to " + server + "! Abandoning batcher.")
       ns.exit()
     }
+  }
+
+  let hackTime = -1
+  let growTime = -1
+  let weakenTime = -1
+  let batchCount = 0
+
+  // Minimize Server Security
+  while(true) {
+    if (
+      ns.getServerSecurityLevel(targetServerName) == ns.getServerMinSecurityLevel(targetServerName)
+    ) {
+      break
+    }
+    await runOnServers(ns, "/hawaii/batch_weaken.js", targetServerName, batchCount, 0, 1)
+    await ns.sleep(stepDelay)
+    batchCount += 1
+  }
+
+  // Kill unneeded spawned prep scripts
+  for (let server of allServersWithRam) {
+    ns.killall(server)
+  }
+
+  // Prep The Server
+  batchCount = 0
+  while(true) {
+    if (
+      ns.getServerSecurityLevel(targetServerName) == ns.getServerMinSecurityLevel(targetServerName) &&
+      ns.getServerMaxMoney(targetServerName) == ns.getServerMoneyAvailable(targetServerName)
+    ) {
+      break
+    }
+    growTime = ns.getGrowTime(targetServerName)
+    weakenTime = ns.getWeakenTime(targetServerName)
+    await runOnServers(ns, "/hawaii/batch_weaken.js", targetServerName, batchCount, 0, 1)
+    await runOnServers(ns, "/hawaii/batch_grow.js", targetServerName, batchCount, (weakenTime - growTime) + (stepDelay), 12)
+    await ns.sleep(stepDelay * 2)
+    batchCount += 1
+  }
+
+  // Kill unneeded spawned prep scripts
+  for (let server of allServersWithRam) {
+    ns.killall(server)
+  }
+
+  // Deploy the HWGW!
+  batchCount = 0
+  while(true) {
+    if (ns.getServerMinSecurityLevel(targetServerName) < ns.getServerSecurityLevel(targetServerName)) {
+      ns.print("WARN: Started new batch run but security is not minimized. This should not happen.")
+    }
+    if (ns.getServerMoneyAvailable(targetServerName) < ns.getServerMaxMoney(targetServerName)) {
+      ns.print("WARN: Started new batch run but money is not maximized. This should not happen.")
+    }
+    hackTime = ns.getHackTime(targetServerName)
+    growTime = ns.getGrowTime(targetServerName)
+    weakenTime = ns.getWeakenTime(targetServerName)
+    await runOnServers(ns, "/hawaii/batch_weaken.js", targetServerName, batchCount, 0, 1)
+    await runOnServers(ns, "/hawaii/batch_weaken.js", targetServerName, batchCount, stepDelay, 1)
+    await runOnServers(ns, "/hawaii/batch_grow.js", targetServerName, batchCount, (weakenTime - growTime) + (stepDelay * 2), 2)
+    await runOnServers(ns, "/hawaii/batch_hack.js", targetServerName, batchCount, (weakenTime - hackTime) + (stepDelay * 3), 1)
+    await ns.sleep(stepDelay * 4)
+    batchCount += 1
   }
 }
